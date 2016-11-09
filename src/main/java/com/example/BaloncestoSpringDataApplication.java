7d50fd57715f1ee54d9bb18c8e5e21df254c5370
apellegero <albertpeye@gmail.com>
2016-11-09 15:51:51 +0100

    Initial commit

---
+++ src/main/java/com/example/BaloncestoSpringDataApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaloncestoSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaloncestoSpringDataApplication.class, args);
+	}
+}
